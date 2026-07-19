17efae4e4c65408f950d581be9b645d20207183b
Moiz Baloch <khanmoaiz682@gmail.com>
2026-07-19 18:12:44 +0500

    Object Tracking and Object Monitoring Pipeline Added - Moiz Baloch

--- src/detector.py
+++ src/detector.py
@@ -1,10 +1,18 @@
-from ultralytics import YOLO # type: ignore
+from ultralytics import YOLO
 
 
 class ObjectDetector:
+
     def __init__(self, model_path="models/yolov8n.pt"):
+
         self.model = YOLO(model_path)
 
     def detect(self, frame):
-        results = self.model(frame, verbose=False)
+
+        results = self.model.track(
+            frame,
+            persist=True,
+            verbose=False
+        )
+
         return results
--- src/renderer.py
+++ src/renderer.py
@@ -20,12 +20,16 @@ class Renderer:
         boxes = results[0].boxes
         names = results[0].names
 
+        displayed_objects = 0
+
         for box in boxes:
 
             confidence = float(box.conf[0])
 
             if confidence < 0.50:
                 continue
+
+            displayed_objects += 1
 
             class_id = int(box.cls[0])
             label = names[class_id]
@@ -42,18 +46,31 @@ class Renderer:
                 2
             )
 
-            text = f"{label} {confidence:.2f}"
+            if box.id is not None:
+                object_id = int(box.id[0])
+                text = f"{label} #{object_id} {confidence:.2f}"
+            else:
+                text = f"{label} {confidence:.2f}"
 
             cv2.putText(
                 frame,
-                f"Objects : {len(boxes)}",
-                (20, 35),
+                text,
+                (x1, max(y1 - 10, 20)),
                 cv2.FONT_HERSHEY_SIMPLEX,
-                0.7,
-                (0, 255, 255),
+                0.6,
+                color,
                 2
             )
 
+        # Draw object count once, outside the loop
+        cv2.putText(
+            frame,
+            f"Objects : {displayed_objects}",
+            (20, 35),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.7,
+            (0, 255, 255),
+            2
+        )
+
         return frame
-    
-    
